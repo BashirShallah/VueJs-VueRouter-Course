cb8c61dc36bc2d62a3a6584f04b5f2438f68c5fa
Bashir Shallah <bashir.shallah@gmail.com>
2018-07-20 13:19:14 +0300

    vue-router: query parameters.

--- src/components/PostPage.vue
+++ src/components/PostPage.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         Post {{id}}
+        <br>
+        Qurey Params:<br>
+        Q: {{$route.query.q}}<br>
+        name: {{$route.query.name}}
+
     </div>
 </template>
 
--- src/components/PostsPage.vue
+++ src/components/PostsPage.vue
@@ -2,9 +2,9 @@
     <div>
         <h1>Posts</h1>
         <ul>
-            <li><router-link to="/posts/1">Post 1</router-link></li>
+            <li><router-link to="/posts/1?q=1&name=test1">Post 1</router-link></li>
             <li>
-                <router-link :to="{name: 'single-post', params: {id:2} }" >
+                <router-link :to="{name: 'single-post', params: {id:2}, query: {q:2,name:'test2'} }" >
                     Post 2
                 </router-link>
             </li>
