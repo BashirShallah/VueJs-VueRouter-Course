e425c3dfe34479313700825a7c157faa4248c08c
Bashir Shallah <bashir.shallah@gmail.com>
2018-07-20 12:52:04 +0300

    Vue Router - Programmatic Navigation.

--- src/components/AboutUsPage.vue
+++ src/components/AboutUsPage.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>About Us Page</h1>
+    <div>
+        <h1>About Us Page</h1>
+
+        <button @click="goToHome">Go To Home After 1 sec</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods:{
+        goToHome: function(){
+            const self = this;
+
+            setTimeout(function(){
+                self.$router.push({path: "/"});
+            }, 1000);
+        }
+    }
+}
+</script>
